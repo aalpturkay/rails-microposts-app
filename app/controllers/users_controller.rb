a290d81ae31d1c2c831508e15488a1583ccab695
aalpturkay <aykutalpturkay@gmail.com>
2022-10-03 21:33:28 +0300

    Add sign-up feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
-  def new
+  
+  def show
+    @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+        :password_confirmation)
+    end
 end
